142a7b94ed11cd81c46b768eca31721e5c596f98
Theo Reinsberg <theo@Stefans-MBP.teksavvy.com>
2021-05-07 14:08:41 -0700

    encode refractor

--- encode.v
+++ encode.v
@@ -3,62 +3,57 @@ module vgob
 import math.bits
 
 pub fn encode<T>(data T) ?[]byte {
-	// $for field in T.fields {
-	// 	$if field.typ is string {
-	// 		// $(string_expr) produces an identifier
-	// 		// result.$(field.name) = encode_string(data, field.name)
-	// 	} $else $if field.typ is int {
-	// 		result << encode_int(data) ?
-	// 	}
-	// }
-	mut res := encode_no_len<T>(data) ?
-	len := encode_uint(u64(res.len)) ?
-	res.prepend(len)
+	mut e := Encoder{}
+	e.encode<T>(data) ?
+	return e.final()
+}
+
+struct Encoder {
+mut:
+	types []byte
+	data  []byte
+}
+
+fn (mut e Encoder) final() ?[]byte {
+	data_end := e.data.len
+	e.uint(u64(e.data.len)) ?
+	mut res := []byte{cap: e.types.len + e.data.len}
+	res << e.data[data_end..]
+	res << e.types
+	res << e.data[..data_end]
 	return res
 }
 
-fn encode_no_len<T>(data T) ?[]byte {
-	mut result := []byte{}
+fn (mut e Encoder) encode<T>(data T) ? {
+	e.typ<T>() ?
+	e.data << 0 // WHY? Go just always adds a zero at the end, but it isn't in the spec (as far as I can tell)
 	$if T is i8 {
-		result << encode_type(.int) ?
-		result << encode_int(data) ?
+		e.int(data) ?
 	} $else $if T is i16 {
-		result << encode_type(.int) ?
-		result << encode_int(data) ?
+		e.int(data) ?
 	} $else $if T is int {
-		result << encode_type(.int) ?
-		result << encode_int(data) ?
+		e.int(data) ?
 	} $else $if T is i64 {
-		result << encode_type(.int) ?
-		result << encode_int(data) ?
+		e.int(data) ?
 	} $else $if T is byte {
-		result << encode_type(.uint) ?
-		result << encode_uint(data) ?
+		e.uint(data) ?
 	} $else $if T is u16 {
-		result << encode_type(.uint) ?
-		result << encode_uint(data) ?
+		e.uint(data) ?
 	} $else $if T is u32 {
-		result << encode_type(.uint) ?
-		result << encode_uint(data) ?
+		e.uint(data) ?
 	} $else $if T is u64 {
-		result << encode_type(.uint) ?
-		result << encode_uint(data) ?
+		e.uint(data) ?
 	} $else $if T is u64 {
-		result << encode_type(.uint) ?
-		result << encode_uint(data) ?
+		e.uint(data) ?
 	} $else $if T is f32 {
-		result << encode_type(.float) ?
-		resudlt << encode_float(data) ?
+		e.float(data) ?
 	} $else $if T is f64 {
-		result << encode_type(.float) ?
-		result << encode_float(data) ?
+		e.float(data) ?
 	} $else $if T is string {
-		result << encode_type(.string) ?
-		result << encode_string(data) ?
+		e.string(data) ?
 	} $else {
 		return error('unknown type')
 	}
-	return result
 }
 
 enum Type {
@@ -81,45 +76,66 @@ enum Type {
 	map_type = 23
 }
 
-fn encode_type(t Type) ?[]byte {
-	mut typ := encode_int(int(t)) ?
-	typ << 0
-	return typ
-}
-
-fn encode_int(x i64) ?[]byte {
-	return encode_uint(if x < 0 { ((~u64(x)) << 1) | 1 } else { (u64(x) << 1) })
-}
-
-fn encode_uint(x u64) ?[]byte {
-	if x < 128 {
-		return [byte(x)]
-	} else if x < 65536 {
-		return [~byte(1), byte(x >> 8), byte(x)]
-	} else if x < 4294967296 {
-		return [~byte(3), byte(x >> 24), byte(x >> 16), byte(x >> 8), byte(x)]
-	} else if x <= 18446744073709551615 {
-		return [~byte(7), byte(x >> 56), byte(x >> 48), byte(x >> 40), byte(x >> 32), byte(x >> 24),
-			byte(x >> 16), byte(x >> 8), byte(x)]
-	} else {
-		return error('too big: $x, ${x < 18446744073709551616}')
+fn (mut e Encoder) typ<T>() ? {
+	$if T is i8 {
+		e.int(int(Type.int)) ?
+	} $else $if T is i16 {
+		e.int(int(Type.int)) ?
+	} $else $if T is int {
+		e.int(int(Type.int)) ?
+	} $else $if T is i64 {
+		e.int(int(Type.int)) ?
+	} $else $if T is byte {
+		e.int(int(Type.uint)) ?
+	} $else $if T is u16 {
+		e.int(int(Type.uint)) ?
+	} $else $if T is u32 {
+		e.int(int(Type.uint)) ?
+	} $else $if T is u64 {
+		e.int(int(Type.uint)) ?
+	} $else $if T is u64 {
+		e.int(int(Type.uint)) ?
+	} $else $if T is f32 {
+		e.int(int(Type.float)) ?
+	} $else $if T is f64 {
+		e.int(int(Type.float)) ?
+	} $else $if T is string {
+		e.int(int(Type.string)) ?
+	} $else {
+		return error('unknown type')
 	}
 }
 
-fn encode_float(x f64) ?[]byte {
-	return encode_uint(bits.reverse_bytes_64(unsafe { FToU{ f: x }.u }))
+fn (mut e Encoder) int(x i64) ? {
+	e.uint(if x < 0 { ((~u64(x)) << 1) | 1 } else { (u64(x) << 1) }) ?
 }
 
-// and back again
+fn (mut e Encoder) uint(x u64) ? {
+	if x < 128 {
+		e.data << [byte(x)]
+	} else if x < 65536 {
+		e.data << [~byte(1), byte(x >> 8), byte(x)]
+	} else if x < 4294967296 {
+		e.data << [~byte(3), byte(x >> 24), byte(x >> 16), byte(x >> 8), byte(x)]
+	} else if x <= 18446744073709551615 {
+		e.data << [~byte(7), byte(x >> 56), byte(x >> 48), byte(x >> 40), byte(x >> 32),
+			byte(x >> 24), byte(x >> 16), byte(x >> 8), byte(x)]
+	} else {
+		return error('too big: $x')
+	}
+}
+
+fn (mut e Encoder) float(x f64) ? {
+	e.uint(bits.reverse_bytes_64(unsafe { FToU{ f: x }.u })) ?
+}
+
+// and back again, in decode.v
 union FToU {
 	f f64
 	u u64
 }
 
-fn encode_string(x string) ?[]byte {
-	mut res := encode_uint(u64(x.len)) ?
-	for c in x {
-		res << c
-	}
-	return res
+fn (mut e Encoder) string(x string) ? {
+	e.uint(u64(x.len)) ?
+	e.data << x.bytes()
 }
--- v.mod
+++ v.mod
@@ -1,7 +1,7 @@
 Module {
 	name: 'vgob'
 	description: 'A V decoder and encoder for the gob data format'
-	version: ''
+	version: '0.0.1'
 	license: 'MIT'
 	dependencies: []
 }
